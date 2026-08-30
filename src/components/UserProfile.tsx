213049465117ef28675646799ac09b778288640c
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-08-30 09:43:01 +0300

    style: add UserProfile component styles

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -22,45 +22,98 @@ const UserProfile = ({ username }: { username: string }) => {
 
   return (
     data && (
-      <div>
-        <img src={data.avatar_url} alt="" width="117" height="117" />
+      <div className="mt-8 md:mt-10 bg-neutral-0 dark:bg-neutral-800 rounded-2xl py-8 px-6 md:p-12 grid md:grid-cols-[auto_1fr] gap-8">
+        <img
+          src={data.avatar_url}
+          alt=""
+          width="117"
+          height="117"
+          className="rounded-full"
+        />
         <div>
-          <h1>{data.name ?? "Not Available"}</h1>
-          <p>@{data.login}</p>
-          <p>Joined {formatDate(data.created_at)}</p>
-          <p>{data.bio ?? "This profile has no bio"}</p>
-          <ul>
-            <li>
-              <span>Repos</span>
-              <span>{data.public_repos}</span>
-            </li>
-            <li>
-              <span>Followers</span>
-              <span>{data.followers}</span>
-            </li>
-            <li>
-              <span>Following</span>
-              <span>{data.following}</span>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <img src={iconLocation} alt="" />
-              <span>{data.location ?? "Not Available"}</span>
-            </li>
-            <li>
-              <img src={iconTwitter} alt="" />
-              <span>{data.twitter_username ?? "Not Available"}</span>
-            </li>
-            <li>
-              <img src={iconWebsite} alt="" />
-              <span>
-                {data.blog.length === 0 ? "Not Available" : data.blog}
+          <div className="flex flex-col md:flex-row md:justify-between">
+            <div>
+              <h1 className="text-neutral-700 dark:text-neutral-0 font-bold text-[1.625rem] leading-[1.2]">
+                {data.name ?? "Name not Available"}
+              </h1>
+              <p className="text-blue-300 dark:text-blue-500 font-normal text-base leading-normal">
+                @{data.login}
+              </p>
+            </div>
+            <p className="text-neutral-500 dark:text-neutral-0 text-[0.9375rem] font-normal leading-normal">
+              Joined {formatDate(data.created_at)}
+            </p>
+          </div>
+          <p className="mt-6 text-neutral-500/75 dark:text-neutral-0/70 text-[0.9375rem] font-normal leading-normal">
+            {data.bio ?? "This profile has no bio"}
+          </p>
+          <ul className="mt-6 bg-neutral-100 dark:bg-neutral-900 rounded-[10px] py-4 px-5 md:px-8 grid gap-4 md:grid-cols-3">
+            <li className="flex flex-col gap-1">
+              <span className="text-neutral-500 dark:text-neutral-0 text-[0.8125rem] font-normal leading-normal">
+                Repos
+              </span>
+              <span className="text-neutral-700 dark:text-neutral-0 font-bold text-[1.375rem] leading-[1.4]">
+                {data.public_repos}
               </span>
             </li>
-            <li>
+            <li className="flex flex-col gap-1">
+              <span className="text-neutral-500 dark:text-neutral-0 text-[0.8125rem] font-normal leading-normal">
+                Followers
+              </span>
+              <span className="text-neutral-700 dark:text-neutral-0 font-bold text-[1.375rem] leading-[1.4]">
+                {data.followers}
+              </span>
+            </li>
+            <li className="flex flex-col gap-1">
+              <span className="text-neutral-500 dark:text-neutral-0 text-[0.8125rem] font-normal leading-normal">
+                Following
+              </span>
+              <span className="text-neutral-700 dark:text-neutral-0 font-bold text-[1.375rem] leading-[1.4]">
+                {data.following}
+              </span>
+            </li>
+          </ul>
+          <ul className="mt-6 grid md:grid-cols-2 gap-4">
+            <li className="flex items-center gap-4">
+              <img src={iconLocation} alt="" />
+              <span
+                className={`${data.location ? "text-neutral-500 dark:text-neutral-0" : "text-neutral-500/70 dark:text-neutral-0/70"}  font-normal leading-normal text-[0.9375rem]`}
+              >
+                {data.location ?? "Not Available"}
+              </span>
+            </li>
+            <li className="flex items-center gap-4">
+              <img src={iconTwitter} alt="" />
+              <span
+                className={`${data.twitter_username ? "text-neutral-500 dark:text-neutral-0" : "text-neutral-500/70 dark:text-neutral-0/70"}  font-normal leading-normal text-[0.9375rem]`}
+              >
+                {data.twitter_username ?? "Not Available"}
+              </span>
+            </li>
+            <li className="flex items-center gap-4">
+              <img src={iconWebsite} alt="" />
+              <span
+                className={`text-neutral-500/70 dark:text-neutral-0/70 font-normal leading-normal text-[0.9375rem]`}
+              >
+                {data.blog.length === 0 ? (
+                  "Not Available"
+                ) : (
+                  <a
+                    href={data.blog}
+                    className="text-neutral-500 dark:text-neutral-0 hover:underline hover:decoration-1 hover:decoration-neutral-500 dark:hover:decoration-neutral-0 rounded-sm focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+                  >
+                    {data.blog}
+                  </a>
+                )}
+              </span>
+            </li>
+            <li className="flex items-center gap-4">
               <img src={iconCompany} alt="" />
-              <span>{data.company ?? "Not Available"}</span>
+              <span
+                className={`${data.company ? "text-neutral-500 dark:text-neutral-0" : "text-neutral-500/70 dark:text-neutral-0/70"}  font-normal leading-normal text-[0.9375rem]`}
+              >
+                {data.company ?? "Not Available"}
+              </span>
             </li>
           </ul>
         </div>
